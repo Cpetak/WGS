3a4a54993713ad4592ab6b7bed11fc712f390cb8
Csenge Petak <csenge.petak@uvm.edu>
2020-11-08 19:25:24 -0500

    Update bayenv_conversion.py

--- bayenv_conversion.py
+++ bayenv_conversion.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
+import numpy as np
 
-files = ['/users/c/p/cpetak/WGS/angsd_new/LOM_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new/TER_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new/BOD_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new/KIB_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new/CAP_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new/FOG_angsd_polysites.mafs']
+files = ['/users/c/p/cpetak/WGS/angsd_new_noout/LOM_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new_noout/TER_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new_noout/BOD_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new_noout/KIB_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new_noout/CAP_angsd_polysites.mafs', '/users/c/p/cpetak/WGS/angsd_new_noout/FOG_angsd_polysites.mafs']
 pops = ["LOM","TER","BOD","KIB","CAP","FOG"]
 data = []
 
@@ -11,18 +12,19 @@ for i in range(len(pops)):
     data.append(frame)
 
 def init_proc_file(data):
-    data['chromo'] = data['chromo'].apply(str)
-    data['position'] = data['position'].apply(str)
-    data['pos'] = data[['chromo', 'position']].apply(lambda x: ''.join(x), axis=1)
-    df = data[["pos","knownEM", "filename", "nInd"]]
-    df["al_count"] = df["knownEM"] * df["nInd"]
-    df["al_count"] = df.al_count.round().astype(int)
-    df["al_count2"] = df["nInd"] - df["al_count"]
-    df = df[["pos","filename", "al_count", "al_count2"]]
-    pop = df["filename"][2]
-    df = df.rename(columns={"al_count": "al_count"+pop, "al_count2": "al_count2"+pop})
-    df = df.drop(['filename'], axis=1)
-    return df
+	data['chromo'] = data['chromo'].apply(str)
+	data['position'] = data['position'].apply(str)
+	data['pos'] = data[['chromo', 'position']].apply(lambda x: ''.join(x), axis=1)
+	df = data[["pos","knownEM", "filename", "nInd"]]
+	df["al_count"] = df["knownEM"] * df["nInd"]
+	df["al_count"] = df.al_count.round().astype(int)
+	df.drop(df[df['al_count'] < 1].index, inplace = True)
+	df["al_count2"] = df["nInd"] - df["al_count"]
+	df = df[["pos","filename", "al_count", "al_count2"]]
+	pop = df["filename"][2]
+	df = df.rename(columns={"al_count": "al_count"+pop, "al_count2": "al_count2"+pop})
+	df = df.drop(['filename'], axis=1)
+	return df
 
 lom_df = init_proc_file(data[0])
 ter_df = init_proc_file(data[1])
@@ -31,7 +33,19 @@ kib_df = init_proc_file(data[3])
 cap_df = init_proc_file(data[4])
 fog_df = init_proc_file(data[5])
 
-merged = pd.merge(lom_df, ter_df, bod_df, kib_df, cap_df, fog_df, on="pos", how="outer")
+merged = pd.merge(lom_df, ter_df, on="pos", how="outer")
+merged = pd.merge(merged, bod_df, on="pos", how="outer")
+merged = pd.merge(merged, kib_df, on="pos", how="outer")
+merged = pd.merge(merged, cap_df, on="pos", how="outer")
+merged = pd.merge(merged, fog_df, on="pos", how="outer")
+
+merged = merged.fillna(0)
+merged['al_count2LOM'] = np.where(merged['al_countLOM'] == 0, 20, merged['al_count2LOM'])
+merged['al_count2TER'] = np.where(merged['al_countTER'] == 0, 20, merged['al_count2TER'])
+merged['al_count2BOD'] = np.where(merged['al_countBOD'] == 0, 20, merged['al_count2BOD'])
+merged['al_count2KIB'] = np.where(merged['al_countKIB'] == 0, 20, merged['al_count2KIB'])
+merged['al_count2CAP'] = np.where(merged['al_countCAP'] == 0, 20, merged['al_count2CAP'])
+merged['al_count2FOG'] = np.where(merged['al_countFOG'] == 0, 20, merged['al_count2FOG'])
 
 final_df = pd.DataFrame([el for el in merged["pos"].values for _ in range(2)])
 final_df = final_df.rename(columns={0: "pos"})
@@ -41,7 +55,6 @@ final_df["BOD"] = [ el for pair in zip(merged["al_countBOD"].values, merged["al_
 final_df["KIB"] = [ el for pair in zip(merged["al_countKIB"].values, merged["al_count2KIB"].values) for el in pair]
 final_df["CAP"] = [ el for pair in zip(merged["al_countCAP"].values, merged["al_count2CAP"].values) for el in pair]
 final_df["FOG"] = [ el for pair in zip(merged["al_countFOG"].values, merged["al_count2FOG"].values) for el in pair]
-final_df = final_df.fillna(0)
 
 final_df["LOM"] = final_df.LOM.round().astype(int)
 final_df["TER"] = final_df.TER.round().astype(int)
@@ -50,6 +63,6 @@ final_df["KIB"] = final_df.KIB.round().astype(int)
 final_df["CAP"] = final_df.CAP.round().astype(int)
 final_df["FOG"] = final_df.FOG.round().astype(int)
 
-final_df = final_df.drop(['pos'], axis=1)
+#final_df = final_df.drop(['pos'], axis=1)
 
-final_df.to_csv('/users/c/p/cpetak/WGS/angsd_new/snpsfile', index=False,sep="\t")
+final_df.to_csv('/users/c/p/cpetak/WGS/angsd_new_noout/snpsfile_withpos', index=False,sep="\t",header=False)
