31566accecde86858dd77d05366c44ed2357a049
Csenge Petak <csenge.petak@uvm.edu>
2021-01-18 16:22:10 -0500

    more cleaning

--- playground/fastqc_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir fastqc
-
-# fastqc is a program we are calling that is already available, followed by input and output location
-fastqc example.fastq -o fastqc/ # because I'm going to be in myresults already
-
-
